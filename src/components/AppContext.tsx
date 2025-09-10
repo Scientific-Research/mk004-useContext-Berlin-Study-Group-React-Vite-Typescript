c4a17985f844f29ed5ca423ffe9317476886af8a
Maximilian <scientificresearch309@gmail.com>
2025-09-11 01:06:07 +0200

    the code with correct typescritp is there

--- src/components/AppContext.tsx
+++ src/components/AppContext.tsx
@@ -1,14 +1,24 @@
 import { useState, useEffect, createContext, ReactNode } from 'react';
 import axios from 'axios';
 
+interface IBook {
+  id: number;
+  idCode: string;
+  title: string;
+  description: string;
+  language: string;
+}
+
 interface IAppContext {
   appTitle: string;
   datePartners: string[];
+  books: IBook[];
 }
 
-const _AppContext = {
+const _defaultContext = {
   appTitle: '',
-  datePartners: ['', ''],
+  datePartners: [],
+  books: [],
 };
 
 interface IAppProvider {
@@ -17,18 +27,23 @@ interface IAppProvider {
 
 const booksURl = 'https://edwardtanguay.vercel.app/share/techBooks.json';
 
-export const AppContext = createContext<IAppContext>(_AppContext);
+export const AppContext = createContext<IAppContext>(_defaultContext);
 
 export const AppProvider = ({ children }: IAppProvider) => {
   const appTitle = 'The Berlin Study Group';
   const datePartners = ['Mary', 'Miriam'];
 
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState<IBook[]>([]);
 
   useEffect(() => {
     (async () => {
-      const response = (await axios.get(booksURl)).data;
-      console.log(response);
+      try {
+        const response = (await axios.get(booksURl)).data;
+        setBooks(response);
+        console.log(books);
+      } catch (error) {
+        console.error('Error fetching books', error);
+      }
     })();
   }, []);
 
@@ -37,6 +52,7 @@ export const AppProvider = ({ children }: IAppProvider) => {
       value={{
         appTitle,
         datePartners,
+        books,
       }}
     >
       {children}
