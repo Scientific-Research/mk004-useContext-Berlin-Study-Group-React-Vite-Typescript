82e1bc84987965e69ea090ab0ec214255bdfcd9d
Maximilian <scientificresearch309@gmail.com>
2025-09-10 12:42:53 +0200

    react-router-dom installed and Routes and Route are there

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 // import './App.css';
+import { NavLink, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>useContext-Berlin-Study-Group-React-Vite-Typescript</h1>
+      <h1>Berlin-Study-Group</h1>
+
+      <nav>
+        <NavLink to="welcome">Welcome</NavLink>
+        <NavLink to="info">Info</NavLink>
+        <NavLink to="about">About</NavLink>
+      </nav>
+
+      <Routes>{/* <Route path='welcome' element={<Page/>}></Route> */}</Routes>
       <h3>welcome to this site.</h3>
     </div>
   );
