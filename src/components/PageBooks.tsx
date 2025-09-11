49776907d648b6b789004ea83be606a2dbe1089e
Maximilian <scientificresearch309@gmail.com>
2025-09-11 19:09:06 +0200

    The books are there with Sass using useContext-AppContext

--- src/components/PageBooks.tsx
+++ src/components/PageBooks.tsx
@@ -18,9 +18,19 @@ interface IAppContext {
 export const PageBooks = () => {
   const { books } = useContext(AppContext);
   return (
-    <>
-      <p>Welcome to the Books page.</p>
-      {books.map((b) => console.log(b.title))}
-    </>
+    <div className="pageBooks">
+      {books.map((book) => (
+        <div className="pageBook">
+          <img
+            src={`https://edwardtanguay.vercel.app/share/images/techBooks/${book.idCode}.jpg`}
+            alt=""
+          />
+          <div className="bookInfo">
+            <div className="title">{book.title}</div>
+            <div className="description">{book.description}</div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 };
