6b7f04e4d439cfd5d06474d97e2a5770f9982823
Maximilian <scientificresearch309@gmail.com>
2025-09-11 11:13:01 +0200

    sending the books to the pageWelcome using useContext - AppContext and displaying the number of them

--- src/components/PageWelcome.tsx
+++ src/components/PageWelcome.tsx
@@ -1,7 +1,20 @@
+import { useContext } from 'react';
+import { AppContext } from './AppContext';
+
 export const PageWelcome = () => {
+  const { books } = useContext(AppContext);
   return (
-    <>
+    <div className="pageWelcome">
       <p>Welcome to this site.</p>
-    </>
+      <div className="welcomePage">
+        {books.length === 0 ? (
+          <p className="loading">Loading...</p>
+        ) : (
+          <p className="club">
+            In our club we are reading {books.length} books.
+          </p>
+        )}
+      </div>
+    </div>
   );
 };
