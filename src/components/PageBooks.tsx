432707f4334feb7960ce81d272fb0f114139c542
Maximilian <scientificresearch309@gmail.com>
2025-09-11 11:57:51 +0200

    The title of all books are there

--- src/components/PageBooks.tsx
+++ src/components/PageBooks.tsx
@@ -1,7 +1,26 @@
+import { useContext } from 'react';
+import { AppContext } from './AppContext';
+
+interface IBook {
+  id: number;
+  idCode: string;
+  title: string;
+  description: string;
+  language: string;
+}
+
+interface IAppContext {
+  appTitle: string;
+  datePartners: string[];
+  books: IBook[];
+}
+
 export const PageBooks = () => {
+  const { books } = useContext(AppContext);
   return (
     <>
       <p>Welcome to the Books page.</p>
+      {books.map((b) => console.log(b.title))}
     </>
   );
 };
