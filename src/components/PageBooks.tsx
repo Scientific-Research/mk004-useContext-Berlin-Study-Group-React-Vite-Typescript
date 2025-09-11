a140e6ad5bf6e841750737eea4f078fd94726621
Maximilian <scientificresearch309@gmail.com>
2025-09-11 21:11:46 +0200

    adding key to the map() in PageBokks.tsx

--- src/components/PageBooks.tsx
+++ src/components/PageBooks.tsx
@@ -3,10 +3,11 @@ import { AppContext } from './AppContext';
 
 export const PageBooks = () => {
   const { books } = useContext(AppContext);
+
   return (
     <div className="pageBooks">
       {books.map((book) => (
-        <div className="pageBook">
+        <div className="pageBook" key={book.id}>
           <img
             src={`https://edwardtanguay.vercel.app/share/images/techBooks/${book.idCode}.jpg`}
             alt=""
